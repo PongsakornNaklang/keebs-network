2bebb737aa92b52fcd125b49521b42463d1edaa5
pongsakorn naklang <PongsakornNaklang03@gmail.com>
2023-08-21 00:16:57 +0700

    remove axios

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,30 @@
 import { IUsers as IUsersForSignUp } from "@/pages/api/signup";
 import { IUsers } from "@/pages/api/user";
-import axios from "axios";
 import { useSession } from "next-auth/react";
 
 export const postSignup = async (data: IUsersForSignUp) => {
-    const response = await axios.post('/api/signup', data);
-    return response
-}
+    const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+};
 
 export const updateProfile = async (payload: Partial<IUsers>) => {
-    const session = useSession()
-    const response = await axios.patch('/api/user', payload, {
+    const session = useSession();
+
+    const response = await fetch('/api/user', {
+        method: 'PATCH',
         headers: {
-            Authorization: `Bearer ${session.data?.accessToken.token}`
-        }
-    })
-    return response
-}
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${session.data?.accessToken.token}`
+        },
+        body: JSON.stringify(payload)
+    });
+
+    return response.json();
+};
